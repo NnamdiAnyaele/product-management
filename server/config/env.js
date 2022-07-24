f0bcee43e71f7790188de253725812a635701a54
Anyaele Nnamdi <nnamdianya2011@gmail.com>
2022-07-24 02:31:20 +0100

    used gmail instead of mailtrap to send emails

--- server/config/env.js
+++ server/config/env.js
@@ -10,4 +10,6 @@ module.exports = {
 	accountSid: process.env.TWILIO_ACCOUNT_SID,
 	authToken: process.env.TWILIO_AUTH_TOKEN,
 	phoneNumber: process.env.TWILIO_PHONE_NUMBER,
+	googleEmailUsername: process.env.GOOGLE_EMAIL_USERNAME,
+	googleEmailPassword: process.env.GOOGLE_EMAIL_PASSWORD,
 };
--- server/utils/sendemail.js
+++ server/utils/sendemail.js
@@ -1,20 +1,21 @@
 const nodemailer = require("nodemailer");
-const { emailUsername, emailPassword } = require("../config/env");
+// const { emailUsername, emailPassword } = require("../config/env");
+const { googleEmailUsername, googleEmailPassword } = require("../config/env");
 
 async function sendEmail(userEmail, subject, mailMessage) {
 	try {
 		let transport = nodemailer.createTransport({
-			host: "smtp.mailtrap.io",
-			port: 2525,
+			host: "smtp.gmail.com",
+			port: 465,
 			auth: {
-				user: emailUsername,
-				pass: emailPassword,
+				user: googleEmailUsername,
+				pass: googleEmailPassword,
 			},
 		});
 
 		const mailOptions = {
 			from: "no-reply@tradedpottask.com",
-			to: userEmail,
+			to: "anyaelennamdi@gmail.com",
 			subject,
 			html: mailMessage,
 		};
